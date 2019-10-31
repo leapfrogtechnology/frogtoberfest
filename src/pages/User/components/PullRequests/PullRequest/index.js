77b798775a2d0a80be0348b871e71dd090d0c91b
Ram Bhandari <rambhandari@lftechnology.com>
2019-10-31 20:03:02 +0545

    [x] Convert stateless component to stateful component. [x] Add fetch api for merged PR.

--- src/pages/User/components/PullRequests/PullRequest/index.js
+++ src/pages/User/components/PullRequests/PullRequest/index.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MergeStatus from './MergeStatus';
 import PullRequestInfo from './PullRequestInfo';
+import { GITHUB_TOKEN } from '../../../../../config';
+import { fetchInfoFromGitHub } from '../../../../../utils/utils';
 
 const ISSUE_STATUS = {
   ALL: 'all',
   OPEN: 'open',
   CLOSED: 'closed'
 };
+/**
+ * Pull Request Component.
+ */
+class PullRequest extends Component {
+  state = {
+    isMerged: false,
+  };
 
-const PullRequest = ({ pullRequest }) => (
-  <div className={`bg-white leading-normal p-4 flex border-b border-grey break-words`}>
-    <MergeStatus open={pullRequest.state === ISSUE_STATUS.OPEN} merged={pullRequest.state === ISSUE_STATUS.CLOSED} />
-    <PullRequestInfo pullRequest={pullRequest} />
-  </div>
-);
+  /**
+   * Life cycle event for componentDidMount.
+   */
+  componentDidMount = async () => {
+    const { pullRequest } = this.props;
+    const pullNumber = pullRequest.number;
+    const splittedPRUrlArray = pullRequest.html_url.split('/');
+    const owner = splittedPRUrlArray[3];
+    const repo = splittedPRUrlArray[4];
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/pulls/${pullNumber}/merge`;
+    try {
+      const merged = await fetchInfoFromGitHub(apiUrl, GITHUB_TOKEN);
+      this.setState({
+        isMerged: merged.ok,
+      });
+    } catch (e) {
+      console.log(e)
+      this.setState({
+        isMerged: false,
+      });
+    }
+  }
+
+  /**
+   * Life cycle event for render.
+   */
+  render() {
+    const { pullRequest } = this.props;
+    return (
+      <div className={`bg-white leading-normal p-4 flex border-b border-grey break-words`}>
+        <MergeStatus open={pullRequest.state === ISSUE_STATUS.OPEN} merged={this.state.isMerged} />
+        <PullRequestInfo pullRequest={pullRequest} />
+      </div>
+    );
+  }
+}
 
 // TODO: Convert to camelCase and enable camelcase rule.
 PullRequest.propTypes = {
